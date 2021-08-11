4e899cef6e0a6a9dbb1e8a02db39d61869356536
Asraelite <markusscully@gmail.com>
2021-08-11 21:52:39 +0200

    Fix indentation

--- public/static/js/tnnuts/main.js
+++ public/static/js/tnnuts/main.js
@@ -3,35 +3,33 @@
 var game;
 
 class Game {
-	constructor() {
-
-	}
+	constructor() {}
 
 	start() {
 		this.canvas = document.getElementById('game');
-        this.babylonEngine = new BABYLON.Engine(this.canvas, true);
+		this.babylonEngine = new BABYLON.Engine(this.canvas, true);
 		this.canvas.width = window.innerWidth;
 		this.canvas.height = window.innerHeight;
 
-	    const scene = new BABYLON.Scene(this.babylonEngine);
-	    const camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
-	    camera.setTarget(BABYLON.Vector3.Zero());
-	    camera.attachControl(this.canvas, true);
-	    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
-	    light.intensity = 0.7;
-	    var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
-	    sphere.position.y = 1;
-	    var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
+		const scene = new BABYLON.Scene(this.babylonEngine);
+		const camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+		camera.setTarget(BABYLON.Vector3.Zero());
+		camera.attachControl(this.canvas, true);
+		var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+		light.intensity = 0.7;
+		var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
+		sphere.position.y = 1;
+		var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
 
 		this.babylonEngine.runRenderLoop(function () {
-            scene.render();
-        });
+			scene.render();
+		});
 
 		window.addEventListener("resize", _ => {
 			this.canvas.width = window.innerWidth;
 			this.canvas.height = window.innerHeight;
-	        this.babylonEngine.resize();
-        });
+			this.babylonEngine.resize();
+		});
 	}
 }
 
